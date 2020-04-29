c19dcc7163d26af0e3f006230df6fd57165ef637
Danik4130 <40251695+Danik4130@users.noreply.github.com>
2020-04-29 15:53:43 +0300

    abc

---
+++ ISP4(2)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Management;
+using System.Text;
+using SystemInfo;
+
+namespace SystemInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Function.ShowSystemInfo();
+            Console.ReadLine();
+        }
+        
+    }
+}
